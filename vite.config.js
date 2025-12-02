3528201d77b394ec615762852125a9866f707b6b
sayan-k-d <sayankumar.d2000@gmail.com>
2025-12-02 22:39:58 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "widget.jsx",
+      name: "HimsChatbot",
+      fileName: "chatbot-widget",
+      formats: ["iife"],
+    },
+  },
+});
